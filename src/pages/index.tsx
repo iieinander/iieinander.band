5a3991bb8a2bccead46b0c12f404b4f0ad899687
Tim Pietrusky <timpietrusky@gmail.com>
2023-02-14 10:51:05 +0100

    feat(favicon): added all files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,28 @@ export default function Home({ events }: HomeProps) {
           content="https://iieinander.band/iieinander_color.png"
         />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="theme-color" content="#000000" />
       </Head>
+
       <main className={styles.main}>
         <div className={styles.logo}>
           <Canvas
